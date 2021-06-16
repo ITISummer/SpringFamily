1f6209db75d05a783c499432a5228da2c8fe7e77
summer <lv.summer@qq.com>
2021-06-16 12:54:12 +0800

    clear up sth

--- SpringSecurity/SpringSecurityDemo1/src/main/java/com/ITIS/SpringSecurityDemo1/mapper/UsersMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ITIS.SpringSecurityDemo1.mapper;
-
-import com.ITIS.SpringSecurityDemo1.entity.Users;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersMapper extends BaseMapper<Users> {
-}
